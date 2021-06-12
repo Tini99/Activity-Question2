72c2e818353385ec0b2a137c8c06a0620fedec05
Tini Gisa <1814759@nzse.ac.nz>
2021-06-12 16:21:59 +1200

    Add project files.

---
+++ Activity Question2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Activity_Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+
+            Console.WriteLine("please enter number1");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter number2");
+            number2 = Convert.ToInt32("Console.ReadLine");
+
+            number1 = number1 + number2;
+            number2 = number1 - number2;
+            number1 = number1 - number2;
+
+            Console.WriteLine("number1 =" + Convert.ToString(number1) + "and number2 =" + Convert.ToString(number2));
+             
+        }
+    }
+}
